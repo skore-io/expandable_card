6012e38ba495c366b8edbc0bf56bd67ef47f5f52
gitDanilo <danilo_cej@hotmail.com>
2019-12-05 15:49:06 -0300

    cp

--- lib/components/expandablecard.dart
+++ lib/components/expandablecard.dart
@@ -114,16 +114,16 @@ class _ExpandableCardState extends State<ExpandableCard> with SingleTickerProvid
 
   void expandCard() {
     if (!_cardIsExpanded) {
+
+      setState(() => _isAnimating = true);
+
       _animationScrollPercent = Tween<double>(begin: _scrollPercent, end: 1.0).animate(
         CurvedAnimation(parent: _animationController, curve: _bounceOutCurve),
       );
 
       _animationController.forward();
 
-      setState(() {
-        _scrollPercent = 1.0;
-        _cardIsExpanded = true;
-      });
+      setState(() => _cardIsExpanded = true);
     }
   }
 
